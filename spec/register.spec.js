2b51721ac641cc15c99d55dc165855498c6f9f3c
Gabriel Burns <payprplayn@gmail.com>
2017-09-09 10:57:39 -0500

    added display method to  Register class which returns "0.00" as default

--- spec/register.spec.js
+++ spec/register.spec.js
@@ -1,4 +1,7 @@
-var Example = require('../lib/register');
+var Register = require('../lib/register');
 
 describe("Register functionality", function() {
+  it("display displays 0 at start", function(){
+    expect((new Register()).display()).toEqual("0.00");
+  })
 });
