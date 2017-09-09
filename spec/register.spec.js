fe896591b50053aa5c8b9a83914d977ae5ab0577
Gabriel Burns <payprplayn@gmail.com>
2017-09-09 11:12:21 -0500

    display msg pt 2

--- spec/register.spec.js
+++ spec/register.spec.js
@@ -1,7 +1,12 @@
 var Register = require('../lib/register');
 
 describe("Register functionality", function() {
-  it("display displays 0 at start", function(){
-    expect((new Register()).display()).toEqual("0.00");
+  it("display retuns 0 at start", function(){
+    expect((new Register()).display()).toEqual("0");
+  })
+  it("display returns 1.00 when total is 1.00", function(){
+    var ourRegister= new Register();
+    ourRegister.alice=100;
+    expect(ourRegister.display()).toEqual("100")
   })
 });
