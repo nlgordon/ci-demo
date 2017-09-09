d320f896d32ead6cb8429d810255bd37d29c1be8
Elizabeth <elizabeth.a.wells1@gmail.com>
2017-09-09 11:11:27 -0500

    First test of addAmount

--- spec/register.spec.js
+++ spec/register.spec.js
@@ -4,4 +4,9 @@ describe("Register functionality", function() {
   it("display displays 0 at start", function(){
     expect((new Register()).display()).toEqual("0.00");
   })
+  it("Adding 0 should not change total", function() {
+      var register = new Register();
+      register.addAmount(0);
+      expect(register.display()).toEqual("0.00");
+  });
 });
